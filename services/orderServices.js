36d8d5c2774a0629aa39e352ce2d85a9762ed7cf
loai tarek <loai.tarek44@gmail.com>
2023-03-26 02:50:24 +0200

    webhookt

--- services/orderServices.js
+++ services/orderServices.js
@@ -127,7 +127,7 @@ exports.checkoutSession = asyncHandler(async (req, res, next) => {
 const createCardOrder = async (session) => {
   const cartId = session.client_reference_id;
   const shippingAddress = session.metadata;
-  const orderPrice = session.display_Items[0].amount / 100;
+  const orderPrice = session.amount_total / 100;
 
   const cart = await Cart.findById(cartId);
   const user = await User.findOne({ email: session.customer_email });
